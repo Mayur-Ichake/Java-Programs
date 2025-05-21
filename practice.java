686bc4fcae68af8d3d576a6029f2eb57dabe6cab
mayur ichake <mayurichake4@gmail.com>
2025-05-21 22:21:05 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -6,7 +6,6 @@ public class practice {
 public static void main(String args []) {
  Scanner sc = new Scanner (System.in);
  String name = sc.nextLine();
- System.out.println(name);
- 
+ System.out.println(name); 
 }
 }
